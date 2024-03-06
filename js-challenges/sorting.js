ffaceee86c1e840819f53f4cdd8a511842b3b668
mucyorene <renemucyomucici@gmail.com>
2024-03-06 17:02:08 +0200

    Added sorting challenge

--- js-challenges/sorting.js
+++ js-challenges/sorting.js
@@ -2,7 +2,7 @@ function checkPrime(num) {
     if (num <= 1) {
         return false;
     }
-    for (let i = 2; i < num; i++) {
+    for (let i = 1; i <= num; i++) {
         if (num % i === 0) {
             return false;
         }
